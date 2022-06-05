a1ffc1da01a2d62674d6657b53b11b88cadb9c57
kan-ledmaster <kan@ledmaster.pro>
2022-06-05 20:56:31 +0700

    MsDataSet experiments

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,33 @@ mod ms_mapping;
 #[cfg(test)]
 mod tests
 {
+    use std::any::{Any, TypeId};
+    use std::{mem, thread};
+    use std::time::Duration;
     use crate::ms_data_set::MsDataSet;
+
+
 
     #[test]
     fn it_works()
     {
         let mut ds = MsDataSet::new();
+        for i in 0..100
+        {
+            ds.add_entity(Box::new(format!("a-{}",i)));
+        }
+
+        for i in 0..100
+        {
+            let e = ds.get_entity::<String>(i);
+            println!("e: {}", *e);
+        }
+
+
         let result = 2 + 2;
         assert_eq!(result, 4);
-        println!("ok")
+        println!("ok");
+        drop(ds);
+        //thread::sleep(Duration::from_secs(5));
     }
 }
--- src/ms_data_set.rs
+++ src/ms_data_set.rs
@@ -1,6 +1,8 @@
 use std::any::{Any, TypeId};
 use std::borrow::BorrowMut;
 use std::collections::HashMap;
+use std::mem;
+use std::mem::swap;
 
 pub struct MsDataSet
 {
@@ -14,25 +16,36 @@ impl MsDataSet
 		Self { entities_map: HashMap::new(), }
 	}
 
-	pub fn get_entities(&mut self, type_id: TypeId) -> &mut Vec<Box<dyn Any>>
+	pub fn get_entity<T: 'static>(&mut self, index: usize) -> &mut Box<T>
 	{
-		let map = self.entities_map.borrow_mut();
+		let type_id = TypeId::of::<T>();
+		let map = &mut self.entities_map;
+		let vec = map.get_mut(&type_id).unwrap();
 
-		return if map.contains_key(&type_id)
+		let mut any_e = vec.get_mut(index).unwrap();
+		unsafe
 		{
-			map.get_mut(&type_id).unwrap()
-		} else
-		{
-			let vec = Vec::new();
-			map.insert(type_id.clone(), vec);
-			map.get_mut(&type_id).unwrap()
+			let tr_result = mem::transmute::<&mut Box<dyn Any>, &mut Box<T>>(any_e);
+			tr_result
 		}
 	}
 
-	pub fn add_entity(&mut self, entity: Box<dyn Any>)
+
+	pub fn add_entity<T: 'static +>(&mut self, entity: Box<T>)
 	{
 		let actual_id = (*entity).type_id();
-		let entities = self.get_entities(actual_id);
+		let entities;
+		match self.entities_map.get_mut(&actual_id)
+		{
+			Some(val) => {entities = val;}
+			None =>
+				{
+					let new_entities: Vec<Box<dyn Any>> = vec![];
+					self.entities_map.insert(actual_id,new_entities);
+					entities = self.entities_map.get_mut(&actual_id).unwrap();
+				}
+		}
+
 		entities.push(entity);
 	}
 }
